566c3dd2e1bad280289eeaf9ba633e00f80069c5
Ankit <ankitmittal0814@gmail.com>
2024-05-16 00:33:31 +0530

    create user controller

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,11 +1,8 @@
 import express from "express";
+import { createUser } from "./userController";
 
 const userRouter = express.Router();
 
-userRouter.post("/register", (req, res) => {
-  res.json({
-    message: "User Registered",
-  });
-});
+userRouter.post("/register", createUser);
 
 export default userRouter;
