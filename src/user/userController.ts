365ced704f276cb383327ac9935681d939ceb511
Ankit <ankitmittal0814@gmail.com>
2024-05-16 21:48:43 +0530

    Add try catch blocks

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //   console.log("Req Body: ", req.body);
@@ -17,30 +18,45 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 
   //search in database if this email already exists
-  const user = await userModel.findOne({ email });
-  if (user) {
-    const error = createHttpError(400, "User already exists with this email");
-    return next(error);
+  try {
+    const user = await userModel.findOne({ email });
+    if (user) {
+      const error = createHttpError(400, "User already exists with this email");
+      return next(error);
+    }
+  } catch (error) {
+    const err = createHttpError(500, "Error while getting user");
+    return next(err);
   }
 
   //hash password
   const hashedPassword = await bcrypt.hash(password, 10);
+
   //store this in database
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
+  let newUser;
+  try {
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while creating user"));
+  }
 
   //token generation
-  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
-    expiresIn: "7d",
-    algorithm: "HS256",
-  });
+  try {
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
 
-  res.json({
-    accessToken: token,
-  });
+    res.json({
+      accessToken: token,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while signing jwt token"));
+  }
 };
 
 // const loginUser = (req: Request, res: Response, next: NextFunction) => {
